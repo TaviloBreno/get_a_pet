90791358c27328cd45a2eb21b169df6865674a48
Tavilo Breno <breno_wk2@hotmail.com>
2024-07-31 18:14:21 -0300

    Configuração do projeto

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,14 @@
 const express = require('express')
 const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+app.use(express.static('public'));
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+})
