c37531957dc160d130870cce26d5475bc25a6b95
Tavilo Breno <breno_wk2@hotmail.com>
2024-07-31 20:15:20 -0300

    Retornando token JWT

---
+++ backend/helpers/create-user-token.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const createUserToken = async (user, req, res) => {
+    const token = jwt.sign({ name: user.name, id: user._id }, process.env.TOKEN_SECRET, {
+        expiresIn: 86400
+    }, "nossosecret");
+
+    res.status(200).json({
+        message: "Você está autenticado",
+        token: token,
+        userId: user._id,
+    })
+};
+
+module.exports = createUserToken;
