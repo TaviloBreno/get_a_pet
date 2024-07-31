0e787e5b838078f3d3ee51f1dc812f812ad86385
Tavilo Breno <breno_wk2@hotmail.com>
2024-07-31 19:22:49 -0300

    Validações de usuários

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,36 @@ const User = require('../models/User');
 
 module.exports = class UserController{
     static async register(req, res){
-        res.json('Olá Get a Pet')
+        const name = req.body.name;
+        const email = req.body.email;
+        const phone = req.body.phone;
+        const password = req.body.password;
+        const confirmPassword = req.body.confirmPassword;
+
+        if(!name){
+            return res.status(400).json({message: 'O nome é obrigatório'});
+        }
+
+        if(!email){
+            return res.status(400).json({message: 'O email é obrigatório'});
+        }
+
+        if(!phone){
+            return res.status(400).json({message: 'O telefone é obrigatório'});
+        }
+
+        if(!password){
+            return res.status(400).json({message: 'A senha é obrigatória'});
+        }
+
+        if(password !== confirmPassword){
+            return res.status(400).json({message: 'As senhas não conferem'});
+        }
+
+        const userExists = await User.findOne({email: email});
+
+        if(userExists){
+            return res.status(400).json({message: 'Email já cadastrado'});
+        }
     }
 }
